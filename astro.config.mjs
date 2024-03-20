d32c97bd51c1401d53f310ec2a3864685705cae2
jay3332 <40323796+jay3332@users.noreply.github.com>
2024-03-19 23:43:11 -0400

    add astro/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://jay3332.tech',
-  integrations: [
-    tailwind({
-      config: { path: 'tailwind.config.cjs', applyBaseStyles: false },
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    config: {
+      path: 'tailwind.config.cjs',
+      applyBaseStyles: false
+    }
+  }), sitemap(), mdx()],
+  output: "server",
+  adapter: cloudflare()
 });
