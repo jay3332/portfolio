210533700e5b1ba8f8f183eea82173c1bf27840c
jay3332 <40323796+jay3332@users.noreply.github.com>
2026-07-20 02:54:02 -0400

    add back cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,9 +53,7 @@ export default defineConfig({
   site: 'https://jay3332.tech',
   integrations: [typstConfig, sitemap(), mdx(mdxConfig), icon()],
   output: "server",
-
-  // adapter: cloudflare(),
   markdown: mdxConfig,
 
-  // adapter: cloudflare(),
+  adapter: cloudflare(),
 });
